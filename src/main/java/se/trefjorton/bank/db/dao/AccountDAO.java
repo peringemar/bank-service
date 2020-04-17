ab875b13d75488f1dde0c569532eca82f60d5724
Per-Ingemar Andersson <Per-Ingemar.Andersson@netent.com>
2020-04-17 08:09:45 +0200

    Minor cleanup

--- src/main/java/se/trefjorton/bank/db/dao/AccountDAO.java
+++ src/main/java/se/trefjorton/bank/db/dao/AccountDAO.java
@@ -12,12 +12,4 @@ public class AccountDAO extends AbstractDAO<AccountEntity> {
     public AccountEntity findById(Long id) {
         return get(id);
     }
-
-//    public long create(Person person) {
-//        return persist(person).getId();
-//    }
-//
-//    public List<Person> findAll() {
-//        return list(namedQuery("com.example.helloworld.core.Person.findAll"));
-//    }
 }
